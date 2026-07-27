f2d5dbcfb816de5642949480beaa6fcb74a38f06
Aditya Shingare <aditya.s1@sankeysolutions.com>
2026-07-27 16:40:37 +0530

    Update health endpoint to test pipeline

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+  res.status(200).json({ status: 'ok - tested via pipeline v2!' });
 });
 
 app.use('/api', greetingRoutes);
