6dffedf5a72f0637a2c002f6881e01df168f0936
Aditya Shingare <aditya.s1@sankeysolutions.com>
2026-07-27 17:43:12 +0530

    Update status to okay

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok - tested via pipeline v2!' });
+  res.status(200).json({ status: 'okay' });
 });
 
 app.use('/api', greetingRoutes);
